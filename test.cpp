c82123cdfd0089bb4dc8323c6e481d9df6e93d2e
yolo. <cauerast@gmail.com>
2025-12-03 14:15:39 -0300

    update

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int arr[] = {0, 1};
+    
+    for(int i = 0; i < 10; i++){
+        int n = arr[i] + arr[i + 1]
+        arr.append(n)
+    }
+    return 0;
+
+    std::cout << arr 
+}
